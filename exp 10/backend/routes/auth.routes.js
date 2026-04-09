40ca0f06a686fa46d66a7442d896165fc0b304f2
Sri Rangan <srirangan0603@gmail.com>
2026-04-09 19:56:31 +0530

    Update auth.routes.js

--- exp 10/backend/routes/auth.routes.js
+++ exp 10/backend/routes/auth.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 const protect = require("../middleware/auth.middleware");
 
+
 router.post("/register", auth.register);
 router.post("/login",    auth.login);
 router.get("/me",        protect, auth.getMe);
