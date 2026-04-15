68928c28e761f63e9389c8103be8e80f3c0e51a7
Sri Rangan <srirangan0603@gmail.com>
2026-04-15 23:37:54 +0530

    Update bookings.routes.js

--- exp 10/backend/routes/bookings.routes.js
+++ exp 10/backend/routes/bookings.routes.js
@@ -3,7 +3,9 @@ const bookings = require("../controllers/bookings.controller");
 const protect = require("../middleware/auth.middleware");
 
 router.post("/",                          protect, bookings.createBooking);
+
 router.get("/my",                         protect, bookings.getMyBookings);
+
 router.get("/:bookingId",                 protect, bookings.getBookingById);
 router.patch("/:bookingId/cancel",        protect, bookings.cancelBooking);
 
