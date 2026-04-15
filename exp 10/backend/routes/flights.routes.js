50fdbaf0badff374276309748011f7cdb04187c1
Sri Rangan <srirangan0603@gmail.com>
2026-04-15 23:38:34 +0530

    Update flights.routes.js

--- exp 10/backend/routes/flights.routes.js
+++ exp 10/backend/routes/flights.routes.js
@@ -3,6 +3,8 @@ const flights = require("../controllers/flights.controller");
 
 router.get("/",       flights.searchFlights);
 router.get("/:id",    flights.getFlightById);
+
+
 router.post("/",      flights.createFlight);   // seed flights
 
 module.exports = router;
