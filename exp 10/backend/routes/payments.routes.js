0039287759f468080c4de632e58e2ad692c9e50f
Sri Rangan <srirangan0603@gmail.com>
2026-04-15 23:38:45 +0530

    Update payments.routes.js

--- exp 10/backend/routes/payments.routes.js
+++ exp 10/backend/routes/payments.routes.js
@@ -3,6 +3,8 @@ const payments = require("../controllers/payments.controller");
 const protect = require("../middleware/auth.middleware");
 
 router.post("/",                          protect, payments.processPayment);
+
 router.get("/booking/:bookingId",         protect, payments.getPaymentByBooking);
+
 
 module.exports = router;
